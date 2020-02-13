0999a4b692e9472e1d6534082e25c98a6289639d
Olivier Vincent <olivier.vincent@univ-lyon1.fr>
2020-02-13 21:36:49 +0100

    Initial commit for the Plot-OV module

---
+++ README.md
@@ -0,0 +1,7 @@
+# plov -- Extra interactive Matplotlib features
+
+- the Line class creates a draggable line on a matplotlib figure/axes
+- the Cursor class creates a cursor (vertical+horizontal dashed lines) that moves with the mouse
+
+Module requirements : matplotlib, numpy
+Python : >= 3.6
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+        name='plov',
+        version="0.1",
+        author='Olivier Vincent',
+        author_email='olivier.vincent@univ-lyon1.fr',
+        url='https://cameleon.univ-lyon1.fr/ovincent/plov',
+        description='Additional interactive features for matplotlib',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        packages=find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6'
+)
