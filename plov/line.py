1f37588eb5aadf802274faa74bc4abb77ddd8004
Olivier Vincent <olivier.vincent@univ-lyon1.fr>
2020-10-01 19:02:33 +0200

    Changed 'picker=' to 'picker=True, pickradius=' following a Matplotlib 3.3 deprecation warning.

--- plov/line.py
+++ plov/line.py
@@ -17,16 +17,16 @@ from .interactive_object import InteractiveObject
 # ================================= Testing ==================================
 
 def main(blit=True, backend=None):  # For testing purposes
-    
+
     if backend is not None:
         matplotlib.use(backend)
-    
+
     """ example of instances of Lines in different figures and axes"""
     fig1, ax1 = plt.subplots()
     tt = np.linspace(0, 4, 100000)
     xx = np.exp(-tt)
     ax1.plot(tt, xx, '-b')
-    
+
     ax1.set_xscale('log')
     ax1.set_yscale('log')
 
@@ -37,7 +37,7 @@ def main(blit=True, backend=None):  # For testing purposes
     fig2, (ax2a, ax2b) = plt.subplots(1, 2)
     ax2a.plot(tt, xx)
     ax2b.plot(z, '-ob')
-    
+
     ax2a.set_yscale('log')
 
     l2 = Line(ax=ax2a)
@@ -46,7 +46,7 @@ def main(blit=True, backend=None):  # For testing purposes
     l4 = Line(blit=blit)  # this one is plot in current axes, and sets the blit behavior of all lines
 
     plt.show(block=False)
-    
+
     return l1, l2, l3, l4
 
 
@@ -55,7 +55,7 @@ def main(blit=True, backend=None):  # For testing purposes
 
 class Line(InteractiveObject):
     """Interactive draggable line in matplotlib figure/axes.
-    
+
     Left click anywhere on the line to drag it, right click to remove it.
 
     Parameters
@@ -75,36 +75,36 @@ class Line(InteractiveObject):
     Appearance of the connecting line:
     - `linestyle` (matplotlib's linestyle, default: continuous '-').
     - `linewidth` (float, default: 1). Line width.
-    
+
     Instanciation option:
     - `avoid_existing` (bool, default:True). Avoid overlapping existing lines
     (only avoids that edge points overlap, but lines can still cross).
-    
+
     Other
     - `blit` (bool, default True). If True, blitting is used for fast rendering
     - `block`(bool, default False). If True, object blocks the console
     (block not implemented yet for Line and Rect)
     """
-    
+
     name = 'Draggable Line'
-    
+
 
     def __init__(self, fig=None, ax=None, pickersize=5, color=None,
                  ptstyle='.', ptsize=5,linestyle='-', linewidth=1,
                  avoid_existing = True, blit=True, block=False):
-        
+
         super().__init__(fig=fig, ax=ax, color=color, blit=blit, block=block)
 
 
         xlim, ylim = self.ax.get_xlim(), self.ax.get_ylim()
-        
-        self.create(pickersize, self.color, ptstyle, ptsize, 
+
+        self.create(pickersize, self.color, ptstyle, ptsize,
                    linestyle, linewidth, avoid_existing)
 
         # to prevent any shift in axes limits when instanciating line. The
         self.ax.set_xlim(xlim)
         self.ax.set_ylim(ylim)
-        
+
         self.fig.canvas.draw()  # Useful?
 
 # ============================ main line methods =============================
@@ -112,38 +112,38 @@ class Line(InteractiveObject):
 # these methods are specific to the Line class and are called by the more
 # generic callback functions (see below)
 
-    def create(self, pickersize, color, ptstyle, ptsize, linestyle, 
+    def create(self, pickersize, color, ptstyle, ptsize, linestyle,
                linewidth, avoid_existing):
         """Create the line and its components (pt1, pt2, link)"""
-   
+
         # set position of line on screen so that it does not overlap others --
         pos = self.__class__.set_initial_position(self, pickersize, avoid_existing)
         (x1, y1), (x2, y2) = pos
 
         # create edge points -------------------------------------------------
         pt1, = self.ax.plot(x1, y1, marker=ptstyle, color=color,
-                            markersize=ptsize, picker=ptsize)
+                            markersize=ptsize, picker=True, pickradius=ptsize)
         pt2, = self.ax.plot(x2, y2, marker=ptstyle, color=color,
-                            markersize=ptsize, picker=ptsize)
+                            markersize=ptsize, picker=True, pickradius=ptsize)
 
         # create connecting line (link) ---------------------------------------
         x1, y1 = self.get_pt_position(pt1, 'data')
         x2, y2 = self.get_pt_position(pt2, 'data')
 
-        link, = self.ax.plot([x1, x2], [y1, y2], picker=pickersize,
-                                  c=color,
-                                  linestyle=linestyle, linewidth=linewidth)
+        link, = self.ax.plot([x1, x2], [y1, y2], c=color,
+                             picker=True, pickradius=pickersize,
+                             linestyle=linestyle, linewidth=linewidth)
 
         # assemble lines and pts into "all" ----------------------------------
         self.all_artists = pt1, pt2, link
         self.all_pts = pt1, pt2
-        
+
 
     def set_initial_position(self, pickersize, avoid=True):
         """Set position of new line, avoiding existing lines if necessary."""
-        
+
         a1, b1, a2, b2 = .2, .2, .8, .8  # relative initial position in axes
-        
+
         xmin, xmax = self.ax.get_xlim()
         ymin, ymax = self.ax.get_ylim()
         # Move into px coordinates to avoid problems with nonlinear axes
@@ -152,21 +152,21 @@ class Line(InteractiveObject):
 
         x1, y1 = (1-a1)*xmin + a1*xmax, (1-b1)*ymin + b1*ymax  # default pos.
         x2, y2 = (1-a2)*xmin + a2*xmax, (1-b2)*ymin + b2*ymax
-        
+
         if avoid:
-            mindist = 3*pickersize  # min distance between pts to avoid overlapping       
+            mindist = 3*pickersize  # min distance between pts to avoid overlapping
             dragonax = []  # list of coords (px) of existing lines in the current axes
-    
+
             otherlines = set(self.class_objects()) - set([self])
             for line in otherlines:
                 # if on same axis, record coords in a list to check overlap later
-                if line.ax is self.ax:  
+                if line.ax is self.ax:
                     pt1, pt2 = line.all_pts
                     x1b, y1b = self.get_pt_position(pt1, 'px')
                     x2b, y2b = self.get_pt_position(pt2, 'px')
                     dragonax.append((x1b, y1b))
                     dragonax.append((x2b, y2b))
-    
+
             while True:
                 for (xb, yb) in dragonax:
                     d1 = m.hypot(x1-xb, y1-yb)
@@ -180,73 +180,73 @@ class Line(InteractiveObject):
                         break
                 else:  # if the inner for loop finishes, it means all points are ok
                     break  # --> exit the outside while loop
-      
+
         return self.pxtodata((x1, y1)), self.pxtodata((x2, y2))
 
 
     def set_active_info(self):
         """Set active/inactive points during motion and detect motion mode."""
         line = self.all_artists[2]
-        
+
         # set which points are active and corresponding motion mode ----------
-        
+
         if len(self.picked_artists) == 1:  # only line selected --> move as a whole
             mode = 'whole'
             active_elements = self.all_artists
-            
+
         elif len(self.picked_artists) == 2:  # line and pt selected --> edge mode
             mode = 'edge'
             active_elements = self.picked_artists
-            
-        else:  # Normally this case shouldn't happen as this method should 
+
+        else:  # Normally this case shouldn't happen as this method should
         # be called only for active objects.
             mode = None
             active_pts = None
-        
+
         active_pts = set(active_elements) - {line}
-        
+
         self.active_info = {'active_pts': active_pts, 'mode': mode}
-    
+
 
     def update_position(self, event):
         """Update object position depending on moving mode and mouse position."""
-        
+
         x, y = event.x, event.y  # pixel coordinates
-        
+
         active_pts = self.active_info['active_pts']
         mode = self.active_info['mode']
-        
+
         # move just one point, the other one stays fixed
         if mode == 'edge':
             [pt] = active_pts  # should be the only pt in active_pts
             self.moving_positions[pt] = x, y
-        
+
         # move the line as a whole in a parallel fashion
         elif mode == 'whole':
             # get where click was initially made and calculate motion
             x0, y0 = self.press_info['click']
             dx, dy = x - x0, y - y0
-            
+
             for pt in active_pts:
                 x0_pt, y0_pt = self.press_info[pt]
-                self.moving_positions[pt] = x0_pt + dx, y0_pt + dy 
+                self.moving_positions[pt] = x0_pt + dx, y0_pt + dy
 
         # now apply the changes to the graph
         for pt in active_pts:
             xnew, ynew = self.pxtodata(self.moving_positions[pt])
             pt.set_data(xnew, ynew)
-        
+
         pt1, pt2, link = self.all_artists
         x1, y1 = self.pxtodata(self.moving_positions[pt1])
         x2, y2 = self.pxtodata(self.moving_positions[pt2])
         link.set_data([x1, x2], [y1, y2])
-        
+
 
 # ============================ callback functions ============================
-            
+
     def on_key_press(self, event):
         pass
-        
+
 # ================================ direct run ================================
 
 if __name__ == '__main__':
--- plov/rectangle.py
+++ plov/rectangle.py
@@ -13,32 +13,32 @@ from .interactive_object import InteractiveObject
 
 
 def main(blit=True, backend=None):  # For testing purposes
-    
+
     if backend is not None:
         matplotlib.use(backend)
-    
+
     fig1, ax1 = plt.subplots()
     tt = np.linspace(0, 4, 100000)
     xx = np.exp(-tt)
     ax1.plot(tt, xx, '-b')
-    
+
     ax1.set_xscale('log')
     ax1.set_yscale('log')
 
     r1 = Rect()
 
     plt.show(block=False)
-    
+
     return r1
-        
+
 
 class Rect(InteractiveObject):
     """Interactive draggable rectangle in matplotlib figure/axes.
-    
+
     Left click to drag rectangle, right click to remove it. Clicking can be
     done on the edges, vertices (corners), or on the center. These clicks
     trigger different modes of motion.
-    
+
     Parameters
     ----------
     All parameters optional so that a rectangle can be created by `Rect()`.
@@ -57,103 +57,103 @@ class Rect(InteractiveObject):
     Appearance of the edges (lines):
     - `linestyle` (matplotlib's linestyle, default: continuous '-').
     - `linewidth` (float, default: 1). Line width.
-    
+
     Other
     - `blit` (bool, default True). If True, blitting is used for fast rendering
     - `block`(bool, default False). If True, object blocks the console
     (block not implemented yet for Line and Rect).
     - `timeout` (float, default 0, i.e. infinite) timeout for blocking.
     """
-    
+
     name = "Draggable Rectangle"
 
-    
-    def __init__(self, fig=None, ax=None, position=None, pickersize=5, 
+
+    def __init__(self, fig=None, ax=None, position=None, pickersize=5,
                  color='r', ptstyle='.', ptsize=5, linestyle='-', linewidth=1,
                  blit=True, block=False, timeout=0):
-        
+
         super().__init__(fig=fig, ax=ax, color=color, blit=blit, block=block)
-        
+
         xlim, ylim = self.ax.get_xlim(), self.ax.get_ylim()
-        
+
         self.create(pickersize, position,
                     color, ptstyle, ptsize, linestyle, linewidth)
-        
+
         # to prevent any shift in axes limits when instanciating line. The
         self.ax.set_xlim(xlim)
         self.ax.set_ylim(ylim)
-        
+
         self.fig.canvas.draw()  # Useful?
-        
+
         if self.block:
             self.fig.canvas.start_event_loop(timeout=timeout)
-        
-        
+
+
     def create(self, pickersize, position,
                color, ptstyle, ptsize, linestyle, linewidth):
-        
+
         positions = self.set_initial_position(position)
         corner_positions = positions[:-1]  # the last one is the center
-        
+
         # Create all vertices (corners) of the rectangle ---------------------
         corners = []
         for pos in corner_positions:
-            pt, = self.ax.plot(*pos, marker=ptstyle, color=color,
-                               markersize=ptsize, picker=pickersize)
+            pt, = self.ax.plot(*pos, marker=ptstyle, color=color, picker=True,
+                               pickradius=pickersize, markersize=ptsize)
             corners.append(pt)
-            
+
         # Create all lines (edges) of the rectangle --------------------------
         lines = []
         for i, pos in enumerate(corner_positions):
             x1, y1 = corner_positions[i-1]
             x2, y2 = pos
-            line, = self.ax.plot([x1, x2], [y1, y2], c=color, 
-                                 picker=pickersize,
+            line, = self.ax.plot([x1, x2], [y1, y2], c=color,
+                                 picker=True, pickradius=pickersize,
                                  linestyle=linestyle, linewidth=linewidth)
             lines.append(line)
-            
+
         # Create center of rectangle -----------------------------------------
         x_center, y_center = positions[-1]
         center, = self.ax.plot(x_center, y_center, marker='+', color=color,
-                               markersize=10, picker=pickersize)
-        
+                               markersize=10, picker=True, pickradius=pickersize)
+
         self.all_artists = (*corners, *lines, center)
         self.all_pts = (*corners, center)
-    
-    
+
+
     def set_initial_position(self, position):
         """Set position of new line, avoiding existing lines if necessary."""
-        
+
         xmin, xmax = self.ax.get_xlim()
         ymin, ymax = self.ax.get_ylim()
-        
+
         # Move into px coordinates to avoid problems with nonlinear axes
         xmin, ymin = self.datatopx((xmin, ymin))
         xmax, ymax = self.datatopx((xmax, ymax))
-        
+
         if position is None:
             w, h = .5, .5 # relative initial width/height of rectangle in axes
             x_left = (1 - w) / 2 * (xmax - xmin) + xmin
-            x_right = (1 + w) / 2 * (xmax - xmin) + xmin 
+            x_right = (1 + w) / 2 * (xmax - xmin) + xmin
             y_low = (1 - h) / 2 * (ymax - ymin) + ymin
             y_high = (1 + h) / 2 * (ymax - ymin) + ymin
         else:
-            x0, y0, width, height = position            
+            x0, y0, width, height = position
             x_left, y_low = self.datatopx((x0, y0))
             x_right, y_high = self.datatopx((x0 + width, y0 + height))
-               
+
         x_center = (x_left + x_right) / 2
         y_center = (y_low + y_high) / 2
-        
+
         pos1 = self.pxtodata((x_left, y_low))
         pos2 = self.pxtodata((x_right, y_low))
         pos3 = self.pxtodata((x_right, y_high))
         pos4 = self.pxtodata((x_left, y_high))
         posc = self.pxtodata((x_center, y_center))
-        
+
         return pos1, pos2, pos3, pos4, posc
-    
-    
+
+
     def get_position(self):
         """Get position (xmin, ymin, width, height) in data coordinates."""
         xs, ys = [], []
@@ -169,145 +169,145 @@ class Rect(InteractiveObject):
                              'because it has been deleted from the figure or '
                               'because figure itself has been closed.')
         w, h = xmax - xmin, ymax - ymin
-        return xmin, ymin, w, h 
-    
-    
+        return xmin, ymin, w, h
+
+
     def set_active_info(self):
         """Set active/inactive points during motion and detect motion mode."""
-        
+
         corners = self.all_artists[:4]  # not all_pts which can contain additional tracking pts
         lines = self.all_artists[4:-1]
         center = self.all_artists[-1]
-        
+
         # set which points are active and corresponding motion mode ----------
-        
+
         if len(self.picked_artists) == 1:  # edge selected
-            
+
             [pickedline] = self.picked_artists
-            
+
             if pickedline == center:  # center has been picked --> solid body motion
                 mode = 'center'
                 active_lines = lines
                 active_pts = self.all_pts
-            
+
             else:  # one of the edges selected --> move that eddge only
-            
+
                 i = lines.index(pickedline)
                 # Distinguish whether line picked is vertical or horizontal
                 mode = 'horz_edge' if i%2 else 'vert_edge'
-                
+
                 # neighbors of that line
                 inext = (i+1)%4
-                iprev = (i-1)%4         
+                iprev = (i-1)%4
                 active_lines = [pickedline, lines[inext], lines[iprev]]
-                
+
                 # points connected to that line
                 active_pts = [corners[iprev], corners[i], center]
-            
-            
+
+
         elif len(self.picked_artists) == 3:  # corner selected --> move corresponding two edges
-            
+
             # find which corner has been selected and set its index as the mode
             for i, pt in enumerate(corners):
                 if pt in self.picked_artists:
                     mode = i
-            
+
             i = mode
             iprev = (i-1)%4
             inext = (i+1)%4
-            
+
             active_pts = [corners[i], corners[iprev], corners[inext], center]
             active_lines = lines  # all lines need to be updated in corner motion
-            
+
         else:  # Normally, this should not happen
             mode = None
-                 
-        self.active_info =  {'active_pts': active_pts, 
-                             'active_lines': active_lines, 
+
+        self.active_info =  {'active_pts': active_pts,
+                             'active_lines': active_lines,
                              'mode': mode}
 
 
     def update_position(self, event):
         """Update object position depending on moving mode and mouse position."""
-        
+
         x, y = event.x, event.y  # pixel positions
-        
+
         corners = self.all_artists[:4]  # not all_pts which can contain additional tracking pts
         lines = self.all_artists[4:-1]
         center = self.all_artists[-1]
-        
+
         active_pts = self.active_info['active_pts']
         active_lines = self.active_info['active_lines']
         mode = self.active_info['mode']
-        
+
         if mode in ['horz_edge', 'vert_edge', 'center']:
-            
+
             # get where click was initially made and calculate motion
             x0, y0 = self.press_info['click']
             dx = x - x0 if mode != 'horz_edge' else 0
             dy = y - y0 if mode != 'vert_edge' else 0
-            
+
             for pt in active_pts:
                 x0_pt, y0_pt = self.press_info[pt]
-                if pt != center:  
+                if pt != center:
                     x_new, y_new = x0_pt + dx, y0_pt + dy
                 else:   # center point
                     norm = 1 if mode =='center' else 0.5
                     x_new = x0_pt + dx * norm
                     y_new = y0_pt + dy * norm
-                self.moving_positions[pt] = x_new, y_new   
-                
+                self.moving_positions[pt] = x_new, y_new
+
         elif mode in range(4):  # corner motion
-       
+
             i = mode
             picked_corner = corners[i]
             next_corner = corners[(i+1)%4]
             prev_corner = corners[(i-1)%4 ]
-            opposite_corner = corners[(i+2)%4 ]  
- 
+            opposite_corner = corners[(i+2)%4 ]
+
             self.moving_positions[picked_corner] = x, y
             xprev, yprev = self.moving_positions[prev_corner]
             xnext, ynext = self.moving_positions[next_corner]
-            
+
             if i%2:  # bottom right corner or top left
                 self.moving_positions[prev_corner] = xprev, y
                 self.moving_positions[next_corner] = x, ynext
             else:  # bottom left or top right corner
                 self.moving_positions[prev_corner] = x, yprev
                 self.moving_positions[next_corner] = xnext, y
-            
+
             # Calculate center pos from picked pt and diagonally opposed one
             x1, y1 = self.moving_positions[picked_corner]
             x2, y2 = self.moving_positions[opposite_corner]
             self.moving_positions[center] = (x1 + x2) / 2, (y1 + y2) / 2
-                
-            
+
+
         # now apply the changes to the graph ---------------------------------
         for pt in active_pts:
             xnew, ynew = self.pxtodata(self.moving_positions[pt])
             pt.set_data(xnew, ynew)
-        
+
         for line in active_lines:
             i = lines.index(line)
             x1, y1 = self.pxtodata(self.moving_positions[corners[i-1]])
             x2, y2 = self.pxtodata(self.moving_positions[corners[i]])
             line.set_data([x1, x2], [y1, y2])
-            
-            
+
+
 # ============================ callback functions ============================
-            
+
     def on_key_press(self, event):
         if event.key == 'enter':
             print('Rectangle position recorded. Rectangle deleted.')
             self.recorded_position = self.get_position()
             self.delete()
-    
-    
+
+
 # ======================== function rinput ===================================
 
 def rinput():
     """Select area of figure with interactive rectangle (enter to validate).
-    
+
     Parameters
     ----------
     None at the moment. Options will be added soon.
@@ -325,8 +325,8 @@ def rinput():
     return position
 
 
-    
+
 # ================================ direct run ================================
-    
+
 if __name__ == '__main__':
     main()
